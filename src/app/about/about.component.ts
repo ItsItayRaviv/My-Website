d2756178bc54c21253b5432f8f807cfc33b8200d
Itay Raviv <ItsItayRaviv@gmail.com>
2023-06-22 22:32:51 +0300

    about is now beutiful cards

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
-  myGmail = "itsitayraviv@gmail.com?subject=Hello Itay!"
+export class AboutComponent implements AfterViewInit {
   gmLogo = "https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_256px.png";
-  insgLogo = "https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-png-full-colour-glyph.png";
+  insgLogo = "https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-social-platform-icon-png-image_6315976.png";
+  gitLogo = "https://cdn-icons-png.flaticon.com/512/25/25231.png";
+
+  private hoveredCards = new Set<Element>();
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    
+    const cards: HTMLElement[] = this.el.nativeElement.querySelectorAll('.card');
+
+    cards.forEach(card => {
+      this.renderer.listen(card, 'mouseover', () => {
+        if (!this.hoveredCards.has(card)) {
+          const backImgContainer = card.querySelector('.back-img-container');
+          const backLink = card.querySelector('.back-link');
+
+          this.renderer.setStyle(backImgContainer, 'transition', '1s ease-in-out');
+          this.renderer.setStyle(backImgContainer, 'transform', 'translate(100%, -50%)');
+          this.renderer.setStyle(backImgContainer, 'width', '50px');
+
+          this.renderer.setStyle(backLink, 'transition', '1s ease-in-out');
+          this.renderer.setStyle(backLink, 'transform', 'translate(-70%, -50%)');
+
+          this.hoveredCards.add(card);
+        }
+      });
+    });
+  }
 }
