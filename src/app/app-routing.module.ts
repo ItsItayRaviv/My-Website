16cb632698b7c807845f6242b2b7e7473557c972
Itay Raviv <ItsItayRaviv@gmail.com>
2023-06-22 22:43:09 +0300

    moved the cards to new component called socials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component'; 
+import { AboutComponent } from './about/about.component';
+import { SocialsComponent } from './socials/socials.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'socials', component: SocialsComponent }
 ];
 
 @NgModule({
---
+++ src/app/socials/socials.component.ts
@@ -0,0 +1,39 @@
+import { Component, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-socials',
+  templateUrl: './socials.component.html',
+  styleUrls: ['./socials.component.css']
+})
+export class SocialsComponent implements AfterViewInit {
+  gmLogo = "https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_256px.png";
+  insgLogo = "https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-social-platform-icon-png-image_6315976.png";
+  gitLogo = "https://cdn-icons-png.flaticon.com/512/25/25231.png";
+
+  private hoveredCards = new Set<Element>();
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    
+    const cards: HTMLElement[] = this.el.nativeElement.querySelectorAll('.card');
+
+    cards.forEach(card => {
+      this.renderer.listen(card, 'mouseover', () => {
+        if (!this.hoveredCards.has(card)) {
+          const backImgContainer = card.querySelector('.back-img-container');
+          const backLink = card.querySelector('.back-link');
+
+          this.renderer.setStyle(backImgContainer, 'transition', '1s ease-in-out');
+          this.renderer.setStyle(backImgContainer, 'transform', 'translate(100%, -50%)');
+          this.renderer.setStyle(backImgContainer, 'width', '50px');
+
+          this.renderer.setStyle(backLink, 'transition', '1s ease-in-out');
+          this.renderer.setStyle(backLink, 'transform', 'translate(-70%, -50%)');
+
+          this.hoveredCards.add(card);
+        }
+      });
+    });
+  }
+}
